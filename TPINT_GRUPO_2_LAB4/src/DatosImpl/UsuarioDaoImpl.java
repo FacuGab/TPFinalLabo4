2b23d766a15186b697ca9f5e81f0c3f7256d40d3
Matias <85851263+falduti90@users.noreply.github.com>
2023-11-02 15:23:14 -0300

    usuariodaoimpl

---
+++ TPINT_GRUPO_2_LAB4/src/DatosImpl/UsuarioDaoImpl.java
@@ -0,0 +1,36 @@
+package DatosImpl;
+
+import Datos.UsuarioDao;
+import Entidad.Usuario;
+
+public  class UsuarioDaoImpl implements UsuarioDao {
+
+	@Override
+	public Usuario ObtenerUsuario(String cont, String nombre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean InsertarUsuario(String apellido, String nombre, String contraseña, int tipo, int estado) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean EditarUsuario(String cont, int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean ElimnarUsuario(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+
+		
+}
