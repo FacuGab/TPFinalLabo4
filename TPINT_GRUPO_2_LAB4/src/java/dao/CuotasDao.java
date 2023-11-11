f9f70a11d146c3a188199d7cc1c9b0948b9bf9dc
Matias <85851263+falduti90@users.noreply.github.com>
2023-11-10 22:23:17 -0300

    prueba 1.000.000

--- TPINT_GRUPO_2_LAB4/src/java/dao/CuotasDao.java
+++
@@ -1,7 +0,0 @@
-package java.dao;
-
-import java.math.BigDecimal;
-
-public interface CuotasDao {
-	public boolean pagarCuota(int NroCuenta,int idCuota, BigDecimal saldo, String detalle);
-}
--- TPINT_GRUPO_2_LAB4/src/java/negocio/CuotasNegocio.java
+++
@@ -1,7 +0,0 @@
-package java.negocio;
-
-import java.math.BigDecimal;
-
-public interface CuotasNegocio {
-	public boolean pagarCuota(int NroCuenta,int idCuota, BigDecimal importe);
-}
